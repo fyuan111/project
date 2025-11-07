600584ba775c37df818bf602b6958abe1c162a3f
fyuan111 <2373825754@qq.com>
2025-11-07 17:40:44 +0800

    Initial commit

--- platforms/common/include/platform/time.h
+++ platforms/common/include/platform/time.h
@@ -1,1 +1,15 @@
-#include 
+#pragma once 
+
+/**
+ * @brief get and set current time from given timer
+ * 
+ */
+#define fml_clock_gettime   sys_clock_gettime
+#define fml_clock_settime  	sys_clock_settime
+
+
+#define fml_sleep  k_sleep
+#define fml_usleep  k_usleep
+#define fml_msleep  k_msleep
+
+
